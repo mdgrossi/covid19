4f647ac21651fe0c5e92371db3e4791cdc39b83a
Matt <matthew.grossi@rsmas.miami.edu>
2020-05-05 23:15:19 -0400

    Fixed figure size for display on github

--- cases.py
+++ cases.py
@@ -25,6 +25,8 @@ regions = [('Florida', 'Miami-Dade'),
            ('Ohio', 'Montgomery'),
            ('Connecticut', 'Litchfield'),
            ('Rhode Island', 'Providence')]
+           
+fontsize=10
 
 for s,c in regions:
     
@@ -43,25 +45,25 @@ for s,c in regions:
         county['newCases'] = county['cases'].diff()
         county['rolling14day'] = county['newCases'].rolling(14).mean()
     
-        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6.4, 8.0), sharex=True)
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(5.4, 7.0), sharex=True)
 
         ax1.plot(state.newCases, '-o', color='black')
         ax1.plot(state['rolling14day'], linewidth=3, color='lightgrey')
         ax1.set_title('Daily New Confirmed Cases\n'\
                       'Data from https://github.com/nytimes/covid-19-data',
-                      fontsize=14)
+                      fontsize=fontsize+2)
         ax1.tick_params(axis='y', labelsize=12)
-        ax1.set_ylabel(s, fontsize=12)
+        ax1.set_ylabel(s, fontsize=fontsize)
         # ax1.spines['top'].set_visible(False)
         # ax1.spines['right'].set_visible(False)
         ax1.grid(b=True, which='major', axis='y', color='lightgrey')
 
         ax2.plot(county['newCases'], '-o', color='black')
         ax2.plot(county['rolling14day'], linewidth=3, color='lightgrey')
-        ax2.set_xlabel('Date', fontsize=12)
-        ax2.tick_params(axis='x', labelsize=12)
+        ax2.set_xlabel('Date', fontsize=fontsize)
+        ax2.tick_params(axis='x', labelsize=12, labelrotation=45)
         ax2.tick_params(axis='y', labelsize=12)
-        ax2.set_ylabel('{} County'.format(c), fontsize=12)
+        ax2.set_ylabel('{} County'.format(c), fontsize=fontsize)
         ax2.xaxis.set_major_locator(mdates.DayLocator(interval=10))
         ax2.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
         # ax2.spines['top'].set_visible(False)
@@ -71,22 +73,22 @@ for s,c in regions:
         custom_lines = [plt.Line2D([0],[0], lw=1, color='black', marker='o'),
                         plt.Line2D([0],[0], lw=3, color='lightgrey')]
         ax2.legend(custom_lines, ['Daily count', '14-day moving average'],
-                   ncol=2, fontsize=12, bbox_to_anchor=(0.9, -0.18),
+                   ncol=2, fontsize=fontsize, bbox_to_anchor=(0.95, -0.22),
                    edgecolor='lightgrey', fancybox=True, shadow=True)
     else:
         
-        fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8))
+        fig, ax = plt.subplots(1, 1, figsize=(5.4, 3.8))
 
         ax.plot(state['newCases'], '-o', color='black', label='Daily count')
         ax.plot(state['rolling14day'], linewidth=3, color='lightgrey',
                 label='14-day moving average')
         ax.set_title('Daily New Confirmed Cases\n'\
                       'Data from https://github.com/nytimes/covid-19-data',
-                      fontsize=14)
-        ax.tick_params(axis='x', labelsize=12)
+                      fontsize=fontsize+2)
+        ax.tick_params(axis='x', labelsize=12, labelrotation=45)
         ax.tick_params(axis='y', labelsize=12)
-        ax.set_xlabel('Date', fontsize=12)
-        ax.set_ylabel(s, fontsize=12)
+        ax.set_xlabel('Date', fontsize=fontsize)
+        ax.set_ylabel(s, fontsize=fontsize)
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=10))
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
         ax.legend(loc='upper left')
