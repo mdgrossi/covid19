8e90e27dc7000092e6de52b8ded1e37edaf6125e
Matt <matthew.grossi@rsmas.miami.edu>
2020-06-15 12:31:32 -0400

    Added effective reproduction rate to normalized plots

--- cases.py
+++ cases.py
@@ -13,13 +13,14 @@ from us_state_abbrev import us_state_abbrev as abbr
 
 # Load data
 urlNYT = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/'
-urlAtl = 'https://covidtracking.com/api/v1/states/'
+urlAtl = 'https://covidtracking.com/api/v1/states/daily.csv'
+urlR0 = 'https://d14wlfuexuxgcm.cloudfront.net/covid/rt.csv'
 byState = pd.read_csv(urlNYT+'us-states.csv', index_col='state',
                       parse_dates=['date'])
 byCnty = pd.read_csv(urlNYT+'us-counties.csv', index_col='state',
                      parse_dates=['date'])
-fullData = pd.read_csv(urlAtl+'daily.csv', index_col='state',
-                       parse_dates=['date'])
+fullData = pd.read_csv(urlAtl, index_col='state', parse_dates=['date'])
+Rnought = pd.read_csv(urlR0, index_col='region', parse_dates=['date'])
 
 # Areas of interest
 regions = [('Florida', 'Miami-Dade'),
@@ -35,6 +36,21 @@ regions = [('Florida', 'Miami-Dade'),
            
 fontsize = 10
 markersize = 2.5
+
+# Add r_e
+def add_r0(fontsize=11):
+    # Conditional coloring for prediction bar
+    def color():
+        return 'darkgreen' if re < 1 else 'darkred'
+    # Legend location
+    re = np.round(Re['mean'].iloc[-1], 2)
+    ax.annotate('Effective reproduction', (180, 113), (180, 113),
+                xycoords='axes points', fontsize=fontsize-2)
+    ax.annotate(r'number: $r_e$ =', (180, 100), (180, 100),
+                xycoords='axes points', fontsize=fontsize-2)
+    ax.annotate(re, (243, 100), (243, 100),
+                xycoords='axes points', fontsize=fontsize-1,
+                color=color())
 
 for s,c in regions:
     
@@ -97,8 +113,8 @@ for s,c in regions:
         ax.plot(state['rolling14day'], linewidth=3, color='lightgrey',
                 label='14-day moving average')
         ax.set_title('Daily New Confirmed Cases\n'\
-                      'Data from https://github.com/nytimes/covid-19-data',
-                      fontsize=fontsize+2)
+                     'Data from https://github.com/nytimes/covid-19-data',
+                      fontsize=fontsize)
         ax.tick_params(axis='x', labelsize=12, labelrotation=45)
         ax.tick_params(axis='y', labelsize=12)
         ax.set_xlabel('Date', fontsize=fontsize)
@@ -134,18 +150,22 @@ for s,c in regions:
     totalPercent = totalPercent[::-1].dropna()
     spl = make_interp_spline(totalPercent.index, totalPercent, k=3)
     totalPercentSmooth = pd.Series(spl(newTimes), index=newTimes)
-    firstDate = totalPercentSmooth.index[0] + pd.Timedelta(days=14)
+    #firstDate = totalPercentSmooth.index[0] + pd.Timedelta(days=14)
+    firstDate = totalPercentSmooth.index[-1] - pd.Timedelta(days=60)
+    
+    # Effective reproduction rate
+    Re = Rnought.loc[abbr[s]]
     
     fig, ax = plt.subplots(1, 1, figsize=(5.4,  3.8))
     ax.plot(dailyPercentSmooth.loc[firstDate:], linewidth=1, color='lightgrey',
             label='Daily positive cases')
     # ax.plot(totalPercentSmooth, linewidth=2, color='darkblue',
     #         label='Rolling total')
-    ax.plot(dailyPercentSmooth.rolling(14*24).mean(), linewidth=2,
-            color='darkred', label='14-day moving average')
+    ax.plot(dailyPercentSmooth.rolling(14*24).mean().loc[firstDate:], 
+            linewidth=2, color='darkred', label='14-day moving average')
     ax.set_title('Daily Percentage of Positive Tests: {}\n'\
-                 'Data from https://covidtracking.com'.format(s),
-                  fontsize=fontsize+2)
+                 'Data from https://covidtracking.com & '\
+                 'https://rt.live'.format(s), fontsize=fontsize)
     ax.tick_params(axis='x', labelsize=12, labelrotation=45)
     ax.tick_params(axis='y', labelsize=12)
     ax.xaxis.set_major_locator(mdates.DayLocator(interval=10))
@@ -153,7 +173,9 @@ for s,c in regions:
     ax.yaxis.set_major_formatter(mtick.PercentFormatter(1, decimals=0))
     ax.set_xlabel('Date', fontsize=fontsize)
     ax.set_ylabel(s, fontsize=fontsize)
-    ax.legend(loc='upper right')
+    leg = ax.legend(loc='upper right')
+    plt.draw()
+    add_r0()
     plt.tight_layout()
     plt.savefig('/Users/mgrossi/Desktop/covid19/plots/normalized-{}.png'\
                 .format(abbr[s]), dpi=175)
